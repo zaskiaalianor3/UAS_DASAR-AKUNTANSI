64fcfbf074ec24625725e542a8d64bc2eb486977
zaskiaalianor3 <zaskiaalianor3@gmail.com>
2025-12-14 15:20:31 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Aplikasi Akuntansi",
+    layout="wide"
+)
+
+st.title("📊 Aplikasi Akuntansi Sederhana")
+st.info("Gunakan menu di sidebar kiri untuk berpindah halaman")
